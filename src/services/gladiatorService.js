3f0b60131f5e0a33228c4981028777e0eddf189f
Nhi Nguyen <64499617+mynhinguyentruong@users.noreply.github.com>
2023-06-28 12:25:53 -0400

    Modified gladiatorService as it's not very reliable

--- src/services/gladiatorService.js
+++ src/services/gladiatorService.js
@@ -1,20 +1,30 @@
 import csvFile from '../assets/gladiators.csv'
 import Papa from 'papaparse';
 
-var records = ""
-Papa.parse(csvFile, {
-    download: true,
-    header: true,
-    complete: function (input) {
-         records = input.data;
-    }
-});
+// var records = ""
+// Papa.parse(csvFile, {
+//     download: true,
+//     header: true,
+//     complete: function (input) {
+//          records = input.data;
+//     }
+// });
 
 const GladiatorService = {
-
-    get: function() {
-        return records;
-    },
+    get: () => {
+        return new Promise((resolve) => {
+            Papa.parse(csvFile, {
+                download: true,
+                header: true,
+                complete: function (input) {
+                     resolve(input.data);
+                }
+            });
+        })
+    }
+    // function() {
+    //     return records;
+    // },
 };
 
 export default GladiatorService;
