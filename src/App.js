8aff4137db5118c4bc3330034577010577bae4ef
Nhi Nguyen <64499617+mynhinguyentruong@users.noreply.github.com>
2023-06-28 10:40:52 -0400

    Init and render UI

--- src/App.js
+++ src/App.js
@@ -4,10 +4,40 @@ import GladiatorService from "./services/gladiatorService.js";
 
 
 function App() {
+
+  const s = GladiatorService.get
+  const a = s()
+  console.log(a);
   return (
     <div className="App">
       <header className="App-header">
-
+      <table class="table-auto">
+  <thead>
+    <tr>
+      <th>Song</th>
+      <th>Artist</th>
+      <th>Year</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <td>The Sliding Mr. Bones (Next Stop, Pottersville)</td>
+      <td>Malcolm Lockyer</td>
+      <td>1961</td>
+    </tr>
+    <tr>
+      <td>Witchy Woman</td>
+      <td>The Eagles</td>
+      <td>1972</td>
+    </tr>
+    <tr>
+      <td>Shining Star</td>
+      <td>Earth, Wind, and Fire</td>
+      <td>1975</td>
+    </tr>
+  </tbody>
+</table>
+      
       </header>
     </div>
   );
