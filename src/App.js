a5607406367c8169b9d6df39b1eb3ab8b0c46337
Nhi Nguyen <64499617+mynhinguyentruong@users.noreply.github.com>
2023-06-28 10:44:14 -0400

    Add tailwind config

--- src/App.js
+++ src/App.js
@@ -11,6 +11,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <h1 className='text-3xl font-bold underline'>Hello</h1>
       <table class="table-auto">
   <thead>
     <tr>
